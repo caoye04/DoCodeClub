3385acffab9d0b41d36936c073d90eec38305152
whs24 <whs24@mails.tsinghua.edu.cn>
2025-07-05 00:21:32 +0800

    王瀚森第二次作业提交

---
+++ 1st_DoCodeClub_202505/hw/wang_hansen/templates/app.py
@@ -0,0 +1,9 @@
+# hello world
+print("Hello, World!")
+# This is a simple Python script that prints "Hello, World!" to the console.
+# It serves as a basic example of how to write and run a Python program.
+# To run this script, save it as app.py and execute it in a Python environment.
+# You can run this script using the command: python app.py
+# This script is part of a code club project and is intended for educational purposes.
+# The script is located in the templates directory of the project.
+# The project is organized in a way that allows for easy expansion and modification.
